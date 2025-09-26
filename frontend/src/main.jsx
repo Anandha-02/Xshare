9a8483c29aabc54fdec8fd12b0703ece9efad41d
22ad0 65 <22ad065@RITDC.COM>
2025-09-26 16:10:02 +0530

    main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./Xshare.jsx";
 
 // Global CSS imports
 import "./styles/home.css";   // Homepage styles
-import "./styles/login.css";  // Login styles
-import "./styles/register.css";
-import "./styles/upload.css";
+// import "./styles/login.css";  // Login styles
+// import "./styles/register.css";
+// import "./styles/upload.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
